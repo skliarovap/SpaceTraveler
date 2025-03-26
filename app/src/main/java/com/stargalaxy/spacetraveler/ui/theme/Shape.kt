98b659975ca4577fe9b38dadfb86fe2cf70e3f1c
skliarovap <iarkova9927@gmail.com>
2025-03-26 17:54:20 +1000

    Theme Commit

--- app/src/main/java/com/stargalaxy/spacetraveler/ui/theme/Shape.kt
+++ app/src/main/java/com/stargalaxy/spacetraveler/ui/theme/Shape.kt
@@ -4,14 +4,14 @@ import androidx.compose.foundation.shape.CornerBasedShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.unit.dp
 
-data class JetTourByPlanets (
+data class JetSpaceTraveler (
     val small:CornerBasedShape,
     val medium:CornerBasedShape,
     val large:CornerBasedShape,
     val dialog: CornerBasedShape
 )
 
-val Shapes = JetTourByPlanets(
+val Shapes = JetSpaceTraveler(
     small = RoundedCornerShape(8.dp),
     medium = RoundedCornerShape(16.dp),
     large = RoundedCornerShape(64.dp) ,
