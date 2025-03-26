0d8c808c863d1b571127a09fa156cf3dbd6feb8b
skliarovap <iarkova9927@gmail.com>
2025-03-26 17:23:56 +1000

    Theme Commit

--- app/src/main/java/com/stargalaxy/spacetraveler/ui/theme/Color.kt
+++ app/src/main/java/com/stargalaxy/spacetraveler/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package com.stargalaxy.spacetraveler.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PrimaryColor = Color(0xFFFFBD13)
+val OnPrimaryColor = Color(0xFF000021)
+val SecondaryColor = Color(0xFF56CB03)
+val OnSecondaryColor = Color(0xFFFFFFFF)
+val SurfaceColor = Color(0xFFFFFFFF)
+val OnSurfaceColor = Color(0xFFA4A8B3)
+val BackgroundColor = Color(0xFFEFFAFD)
+val OnBackgroundColor = Color(0xFF071738)
--- app/src/main/java/com/stargalaxy/spacetraveler/ui/theme/Type.kt
+++ app/src/main/java/com/stargalaxy/spacetraveler/ui/theme/Type.kt
@@ -1,10 +1,19 @@
 package com.stargalaxy.spacetraveler.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import com.stargalaxy.spacetraveler.R
+
+val fonts = FontFamily(
+    Font(resId = R.font.roboto_regular, weight = FontWeight.Normal),
+    Font(resId = R.font.roboto_medium, weight = FontWeight.Medium),
+    Font(resId = R.font.roboto_bold, weight = FontWeight.Bold),
+    Font(resId = R.font.roboto_light, weight = FontWeight.Light)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
